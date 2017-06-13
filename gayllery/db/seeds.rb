efb60d62c942cd659afc2bbbe8d57e6137fc6c80
Madeline <madelinenelsonfolkersen@gmail.com>
2017-06-13 14:48:02 -0400

    created seed file

--- gayllery/db/seeds.rb
+++ gayllery/db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times {User.create!(firstName:"John",lastName:"Smith")}
+10.times {Collection.create!(name:"collection",description:"This is a test collection")}
+10.times {Image.create!(name:"image",description:"This is a test image",src:"http://via.placeholder.com/350x350
+")}
